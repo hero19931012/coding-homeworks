d064183fd71a811d18dc12aa1cd1b2b36fcaaa7e
Bin <k1234988@gmail.com>
2022-10-12 00:18:01 +0800

    changesomething

--- homeworks/hw3-array/for-each.js
+++ homeworks/hw3-array/for-each.js
@@ -1,24 +1,20 @@
-arr1 = []; //空陣列
-function push1(x){
-    return arr1.push(x)
-}  //Push進arr1
+arr1 = []; //空陣列(x)
 for (i = 0 ;i<10 ; i++){
-   push1(Math.floor(Math.random()*100))
+   arr1.push(Math.floor(Math.random()*100))
 } //10次 1到100隨機亂數迴圈
-// console.log(arr1)
 
-
-
-
-
-var n = 0; //計次
-function run1(num) {
+function foreach(array,func){
+    for(i= 0;i<array.length;i++){
+        func(array[i])
+    }
+}
+function func(num) {
     console.log(num);
-    n ++ ; 
-    console.log(`第${n}次`)
+    console.log(`第${i+1}次`)
+   
 } // print
-function forEach(array, fuc){
-    array.forEach(element =>fuc(element));
-} //for each函數
-forEach(arr1,run1);
+// function forEach(array, fuc){
+    // array.forEach(element =>fuc(element));
+// } //for each函數
+foreach(arr1,func);
 // 
--- homeworks/hw3-array/mapp.js
+++ homeworks/hw3-array/mapp.js
@@ -1,10 +1,12 @@
 const arr1 = [2,4,5,6,7] //data
-let arr2 = [] //新陣列
+let arr2 = []
 function cb1 (num){
-    return num *3
+     num *2
 } //map要執行的function
 function map(array, callback){
-    return arr2 = array.map(callback)
+    for (let key in array){
+        arr2[key] = callback(arr1[key])
+    }
 } // map主程式
 
 map (arr1,cb1); //call map function
