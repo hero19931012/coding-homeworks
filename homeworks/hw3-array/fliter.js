ee9dcae636731011382934c76e2805a70193a7f9
Bin <k1234988@gmail.com>
2022-10-12 14:25:51 +0800

    'Hw03Done

--- homeworks/hw3-array/fliter.js
+++ homeworks/hw3-array/fliter.js
@@ -1,11 +1,20 @@
 const arr1 = [2 ,5,3,6,7]
 let ans =[]
 function cb1(num){
-    return num >= 5
+    if (num >= 5){
+        ans.push(num)
+    }
 }
 function filter1(array,cbfilter){
-    ans = array.filter(cbfilter)
-    return ans 
+    for (let key in array){
+        cb1(array[key])
+        console.log(array[key])
+    }
 }
 filter1(arr1,cb1)
-console.log(ans)
+
+// 請設計一個 function `filter`
+// - 接受 2 個參數，一個陣列與一個
+// - 回傳一個新的陣列
+// - 回傳陣列的每個元素皆符合 callback 裡面條
+// - 答案寫在 filter.js
--- homeworks/hw3-array/slice.js
+++ homeworks/hw3-array/slice.js
@@ -1,11 +1,14 @@
 const arr1 = [1,2,4,5,7,9,11]
 const start = 0 //陣列第一位為0
 const end = arr1.length-1 //最後兩位
+const ans =[]
 
 
 function slice(array, start ,end){
-    console.log(array.slice(start,end))
-    
+  for (let i = start ; i<=end-1 ; i++){
+          ans.push(array[i])
+  }
 }
 slice(arr1,start,end)
-// const ans = slice(arr1,start,end)
+console.log(ans)
+// console.log(end)
